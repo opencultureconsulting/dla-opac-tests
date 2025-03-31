71b771e3065f97aa37415557cbe82e6d5ba80384
Felix Lohmeier <mail@felixlohmeier.de>
2025-03-31 10:07:13 +0200

    Test Detail Kafka: Fragment entfernt

--- tests/detail.spec.js
+++ tests/detail.spec.js
@@ -1,12 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-// Selector für den "Suche starten"-Button
-const selector_button_search = 'text=Weitere Details';
-
 test('kafka', async ({ page }) => {
   await page.goto('https://www.dla-marbach.de/find/opac/id/PE00001005');
-  await page.click(selector_button_search);
   await page.waitForLoadState();
   expect(await page.content()).toMatch('Tschechische Republik');
 });
