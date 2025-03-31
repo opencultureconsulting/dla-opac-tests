7224e163b52c6f62da7c2c85e49973796d190ffe
Felix Lohmeier <mail@felixlohmeier.de>
2025-03-31 16:08:00 +0200

    Fix Test Bestandsübersicht 2. Versuch

--- tests/bestandsübersicht.spec.js
+++ tests/bestandsübersicht.spec.js
@@ -1,10 +1,11 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('kafka', async ({ page }) => {
   await page.goto('https://www.dla-marbach.de/bestandsuebersicht');
   await page.getByRole('textbox', { name: 'Suche in der Bestandsübersicht' }).click();
   await page.getByRole('textbox', { name: 'Suche in der Bestandsübersicht' }).fill('kafka');
+  await page.getByRole('article').filter({ hasText: 'K' }).getByRole('link').click()
   await page.locator('#PE00001005').getByRole('link', { name: '' }).click();
   await expect(page.locator('#BF00000718')).toContainText('Sammlung, Handschriftensammlung [A:Kafka, Franz]');
 });
